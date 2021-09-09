f78b6ef78af461292ed5dfd6301789aa2f9deaae
전상현-MacbookAir <shock@docs.kr>
2021-09-09 18:22:43 +0900

    Initial Commit

---
+++ Shared/MontereyToolbarBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  MontereyToolbarBugApp.swift
+//  Shared
+//
+//  Created by 전상현 on 2021/09/09.
+//
+
+import SwiftUI
+
+@main
+struct MontereyToolbarBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
